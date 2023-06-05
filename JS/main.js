9173892d06b354082d654571805be2d6e2bcfe76
Jorgew1227 <135659798+Jorgew1227@users.noreply.github.com>
2023-06-05 21:48:27 +0100

    Commit Inicial
    Primeiro site

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+$('.Slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveWidth: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+  $('.fade').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
